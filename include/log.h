c0e32e3df9a58e37d63a3a1d54ca7ac0ee6a1ba1
ziv <zivkraus7@gmail.com>
2018-12-06 23:05:56 +0200

    Fixed again the logs function. and again write them in macro style.

--- include/log.h
+++ include/log.h
@@ -8,30 +8,48 @@
 #include "misc.h"
 
 
-#define logger(...) __logger(log_window, ##__VA_ARGS__)
-#define error_logger(...) __error_logger(log_window, ##__VA_ARGS__)
-static inline void __logger(WINDOW * log_window, char * fmt, ...)
-{
-	#ifdef _DEBUG
-	va_list args;
-	va_start(args, fmt);
-	scroll(log_window);
-	mvwprintw(log_window, 8, 1, fmt, args);
-	#endif
+// #define logger(...) __logger(log_window, ##__VA_ARGS__)
+// #define error_logger(...) __error_logger(log_window, ##__VA_ARGS__)
+// static inline void __logger(WINDOW * log_window, char * fmt, ...)
+// {
+// 	#ifdef _DEBUG
+// 	va_list args;
+// 	va_start(args, fmt);
+// 	scroll(log_window);
+// 	mvwprintw(log_window, 8, 1, fmt, args);
+// 	va_end(args);
+// 	#endif
+// }
+
+// static inline void __error_logger(WINDOW * log_window, char * fmt, ...)
+// {
+// 	#ifdef _DEBUG
+// 	va_list args;
+// 	va_start(args, fmt);
+// 	scroll(log_window);
+// 	mvwprintw(log_window, 8, 1, fmt, args);
+// 	va_end(args);
+// 	wrefresh(log_window);
+// 	pause_execution();
+// 	#endif
+// }
+
+#ifdef _DEBUG
+#define error_logger(...) \
+{ \
+	scroll(log_window); \
+	mvwprintw(log_window, 8, 1, ##__VA_ARGS__); \
+	wrefresh(log_window); \
+	pause_execution(); \
 }
 
-static inline void __error_logger(WINDOW * log_window, char * fmt, ...)
-{
-	#ifdef _DEBUG
-	va_list args;
-	va_start(args, fmt);
-	// nodelay(stdscr, FALSE);
-	scroll(log_window);
-	mvwprintw(log_window, 8, 1, fmt, args);
-	wrefresh(log_window);
-	pause_execution();
-	// nodelay(stdscr, TRUE);
-	#endif
+#define logger(...) \
+{ \
+	scroll(log_window); \
+	mvwprintw(log_window, 8, 1, ##__VA_ARGS__); \
 }
-
+#else
+#define error_logger(...)
+#define logger(...) 
+#endif
 #endif
